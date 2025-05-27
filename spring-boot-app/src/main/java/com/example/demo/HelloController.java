ff7a86c255410a7f75193b8602c91222f7572879
Kushagra Gupta <134503681+kushagra-g9@users.noreply.github.com>
2025-05-27 14:19:09 +0530

    Update HelloController.java

--- spring-boot-app/src/main/java/com/example/demo/HelloController.java
+++ spring-boot-app/src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring Boot! from Kushagra Gupta Devops Engineer || Testing my Automation Project | Updating Image Via Argocd | Checking Version is everything  working perfectly";
+        return "Hello, Vishal Bhaiya Spring Boot! from Kushagra Gupta Devops Engineer || Testing my Automation Project | Updating Image Via Argocd | Checking Version is everything  working perfectly";
         
     }
 }
